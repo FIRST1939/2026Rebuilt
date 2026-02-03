3f4d4f7b3c55c0cac38e1105d2a49fa559d771f4
AmeliaK550 <akalahurka10@gmail.com>
2026-02-03 17:53:09 -0600

    Update ClimberIOHardware.java

--- src/main/java/frc/robot/subsystems/climber/ClimberIOHardware.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOHardware.java
@@ -1,8 +1,9 @@
 package frc.robot.subsystems.climber;
 
 import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class ClimberIOHardware implements ClimberIO {
     
-    protected final SparkFlex m_motor;
+    protected final SparkFlex m_motor = new SparkFlex(ClimberConstants.kClimberCAN,MotorType.kBrushless);
 }
