c1728ccb799a8bf84a55135144072bc51d65e07a
aliakbarkhumri <aliakbar.khumri@barstowschool.org>
2026-02-03 17:51:53 -0600

    Update ShooterConstants.java

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -1,14 +1,14 @@
 package frc.robot.subsystems.shooter;
 
 public class ShooterConstants {
-    public static final int flywheelOneCan = 35;
-    public static final double flywheelOneGearReduction = (1);
-    public static final boolean flywheelOneInverted = true;
+    public static final int kFlywheelOneCAN = 35;
+    public static final double kFlywheelOneGearReduction = (1);
+    public static final boolean kFlywheelOneInverted = true;
 
-    public static final int flywheelTwoCan = 36;
-    public static final double flywheelTwoGearReduction = (1);
-    public static final boolean flywheelTwoInverted = false;
+    public static final int kFlywheelTwoCAN = 36;
+    public static final double kFlywheelTwoGearReduction = (1);
+    public static final boolean kFlywheelTwoInverted = false;
 
-    public static final int hoodCan = 37;
-    public static final double hoodGearReduction = ((68.0 / 18.0) * (18.0 / 18.0) * (320.0 / 28.0));
+    public static final int kHoodCAN = 37;
+    public static final double kHoodGearReduction = ((68.0 / 18.0) * (18.0 / 18.0) * (320.0 / 28.0));
 }
