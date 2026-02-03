bbb50c0e2c3b0edd4c3804995ff89064cd980b8e
aliakbarkhumri <aliakbar.khumri@barstowschool.org>
2026-02-03 17:25:32 -0600

    base shooter subsystem definition

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -3,5 +3,21 @@ package frc.robot.subsystems.shooter;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
+
+    public void setShooterVelocity () {
+
+    }
+
+    public void getShooterVelocity () {
+
+    }
+
+    public void setHoodPosition () {
+
+    }
+
+    public void getHoodPosition () {
+        
+    }
     
 }
