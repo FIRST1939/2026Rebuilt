0f3bdd99320b1c7632ec0f46e62dcf1935558d0b
AmeliaK550 <akalahurka10@gmail.com>
2026-02-03 17:30:19 -0600

    Climber set position

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -3,5 +3,12 @@ package frc.robot.subsystems.climber;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
-    
+
+    public void setClimberPosition() {
+        
+    }
+
 }
+
+
+
