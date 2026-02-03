5ff1e9c8a542e34a44a1e7f86a8c32505d26c2b2
Maaz-Hawa <156148130+Maaz-Hawa@users.noreply.github.com>
2026-02-03 17:28:39 -0600

    base subsystem definitions

--- src/main/java/frc/robot/subsystems/spindexer/Spindexer.java
+++ src/main/java/frc/robot/subsystems/spindexer/Spindexer.java
@@ -3,5 +3,21 @@ package frc.robot.subsystems.spindexer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Spindexer extends SubsystemBase {
+ 
+    public void setSpindexerVelocity () {
+
+    }
     
+    public void getSpindexerVelocity () {
+    
+    }
+
+    public void setSpindexerPercentage () {
+    
+    }
+
+    public void getSpindexerPercentage () {
+    
+    }
+
 }
