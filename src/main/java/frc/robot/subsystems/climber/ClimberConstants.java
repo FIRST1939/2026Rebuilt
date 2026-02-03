72bca402a8b4b4b113481d31c3e4c55ac3d91b4a
AmeliaK550 <akalahurka10@gmail.com>
2026-02-03 17:54:44 -0600

    Update ClimberConstants.java

---
+++ src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.climber;
+
+public class ClimberConstants {
+
+    public static final int kClimberCAN = 38;
+    public static final double kClimberGearReduction = (1.0 / 9.0);
+    
+}
