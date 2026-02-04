130e21d31b08812230414269d69a986477ae37c3
StarbuckBarista <66986004+StarbuckBarista@users.noreply.github.com>
2026-02-03 18:50:07 -0600

    High-Level Subsystem Example

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -1,15 +1,32 @@
 package frc.robot.subsystems.climber;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
 
-    public void setClimberPercentage() {
+    private final ClimberIO m_io;
+    private final ClimberIOInputsAutoLogged m_inputs = new ClimberIOInputsAutoLogged();
+
+    public Climber (ClimberIO io) {
+
+        m_io = io;
+    }
+
+    @Override
+    public void periodic () {
+
+        m_io.updateInputs(m_inputs);
+        Logger.processInputs("Climber", m_inputs);
+    }
+
+    public void setClimberPercentage (double percent) {
         
+        m_io.setClimberPercentage(percent);
     }
     
     public void setClimberPosition() {
-        
     }
 
 }
