99a77bad588223e93d57425f3704bf58c3b94b7e
1939DriverStation <barstowrobotics1939@gmail.com>
2026-02-03 18:32:38 -0600

    Add constants and robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,24 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Spindexer.runSpindexer;
+import frc.robot.subsystems.spindexer.Spindexer;
 
 public class RobotContainer {
+  private final Spindexer spindexer = new Spindexer ();
+  private final runSpindexer spindexer_cmd = new runSpindexer(spindexer, 0.5);
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+
+    m_driverController.b().whileTrue(spindexer_cmd);
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
