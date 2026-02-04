85cdf94ed991010465a76961a04ff0a35091e16d
AmeliaK550 <akalahurka10@gmail.com>
2026-02-03 18:14:08 -0600

    Update FeederIOHardware.java

--- src/main/java/frc/robot/subsystems/feeder/FeederIOHardware.java
+++ src/main/java/frc/robot/subsystems/feeder/FeederIOHardware.java
@@ -1,8 +1,28 @@
 package frc.robot.subsystems.feeder;
 
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkFlex;
 
 public class FeederIOHardware implements FeederIO {
     
     protected final SparkFlex m_motor = new SparkFlex(0, null);
+     protected final RelativeEncoder m_encoder = m_motor.getEncoder();
+
+      @Override
+    public void updateInputs (FeederIOInputs inputs) {
+
+        inputs.feederPosition = m_encoder.getPosition();
+        inputs.feederVelocity = m_encoder.getVelocity();
+        inputs.feederVoltage = m_motor.getAppliedOutput() * m_motor.getBusVoltage();
+        inputs.feederCurrent = m_motor.getOutputCurrent();
+        inputs.feederTemperature = m_motor.getMotorTemperature();
+    }
+
+    @Override
+    public void setFeederPercentage (double percent) {
+
+        m_motor.set(percent);
+    }
 }
+
+
