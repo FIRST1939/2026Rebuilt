c381b7383856c7fddf503aeab14a64cc838fc22a
StarbuckBarista <66986004+StarbuckBarista@users.noreply.github.com>
2026-02-03 18:08:09 -0600

    Climber Hardware Example

--- src/main/java/frc/robot/subsystems/climber/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO.java
@@ -15,6 +15,6 @@ public interface ClimberIO {
     }
 
     public default void updateInputs (ClimberIOInputs inputs) {}
-    public default void setClimerPercentage (double percent) {}
+    public default void setClimberPercentage (double percent) {}
     public default void setClimberPosition (double position) {}
 }
--- src/main/java/frc/robot/subsystems/climber/ClimberIOHardware.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOHardware.java
@@ -1,9 +1,27 @@
 package frc.robot.subsystems.climber;
 
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class ClimberIOHardware implements ClimberIO {
     
     protected final SparkFlex m_motor = new SparkFlex(ClimberConstants.kClimberCAN,MotorType.kBrushless);
+    protected final RelativeEncoder m_encoder = m_motor.getEncoder();
+
+    @Override
+    public void updateInputs (ClimberIOInputs inputs) {
+
+        inputs.climberPosition = m_encoder.getPosition();
+        inputs.climberVelocity = m_encoder.getVelocity();
+        inputs.climberVoltage = m_motor.getAppliedOutput() * m_motor.getBusVoltage();
+        inputs.climberCurrent = m_motor.getOutputCurrent();
+        inputs.climberTemperature = m_motor.getMotorTemperature();
+    }
+
+    @Override
+    public void setClimberPercentage (double percent) {
+
+        m_motor.set(percent);
+    }
 }
