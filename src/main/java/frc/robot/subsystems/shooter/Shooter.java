25d7337df55972c36699f06aa2910aeabed9b834
aliakbarkhumri <aliakbar.khumri@barstowschool.org>
2026-02-03 19:40:35 -0600

    Created Shooter object constructer and periodic method

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,14 +1,31 @@
 package frc.robot.subsystems.shooter;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.climber.ClimberIO;
+import frc.robot.subsystems.climber.ClimberIOInputsAutoLogged;
+
 
 public class Shooter extends SubsystemBase {
 
-    public void setFlywheelPercentage () {
-        
+    private final ShooterIO m_io;
+    private final ShooterIOInputsAutoLogged m_inputs = new ShooterIOInputsAutoLogged();
+
+    public Shooter (ShooterIO io) {
+        m_io = io;
     }
 
-    public void setFlywheelVelocity () {
+    public void periodic () {
+
+        m_io.updateInputs(m_inputs);
+        Logger.processInputs("Shooter", m_inputs);
+    }
+
+    public void setFlywheelPercentage (double percent) {
+    }
+
+    public void setFlywheelVelocity (double percent) {
 
     }
 
@@ -16,11 +33,11 @@ public class Shooter extends SubsystemBase {
 
     }
 
-    public void setHoodPercentage () {
+    public void setHoodPercentage (double percent) {
         
     }
 
-    public void setHoodPosition () {
+    public void setHoodPosition (double percent) {
 
     }
 
