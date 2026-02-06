28bcec88e68a14ad949d949bcbe919f1a3a933e8
aliakbarkhumri <aliakbar.khumri@barstowschool.org>
2026-02-05 18:30:59 -0600

    Configured shooter motors

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -1,12 +1,16 @@
 package frc.robot.subsystems.shooter;
 
 public class ShooterConstants {
+        public static final double kFlywheelGearReduction = (1);
+
+    
     public static final int kFlywheelLeaderCAN = 35;
-    public static final double kFlywheelLeaderGearReduction = (1);
+    public static final double kFlywheelLeaderGearReduction = kFlywheelGearReduction;
     public static final boolean kFlywheelLeaderInverted = true;
+    public static final int kFlywheelLeaderCurrentLimit = 120;
 
     public static final int kFlywheelFollowerCAN = 36;
-    public static final double kFlywheelFollowerGearReduction = (1);
+    public static final double kFlywheelFollowerGearReduction = kFlywheelGearReduction;
     public static final boolean kFlywheelFollowerInverted = false;
 
     public static final int kHoodCAN = 37;
