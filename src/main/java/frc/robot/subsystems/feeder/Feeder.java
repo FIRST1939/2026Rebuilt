26a11ca8e6cd8aa39bd3d49d69a63bd5113216c0
AmeliaK550 <akalahurka10@gmail.com>
2026-02-05 18:00:58 -0600

    feeder.java

--- src/main/java/frc/robot/subsystems/feeder/Feeder.java
+++ src/main/java/frc/robot/subsystems/feeder/Feeder.java
@@ -1,18 +1,39 @@
 package frc.robot.subsystems.feeder;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Feeder extends SubsystemBase {
     
-    public void setFeederPercentage () {
+    private final FeederIO m_io;
+    private final FeederIOInputsAutoLogged m_inputs = new FeederIOInputsAutoLogged();
+
+    public Feeder (FeederIO io) {
+        
+        m_io = io;
+    }
+
+    @Override
+
+    public void periodic () {
+
+        m_io.updateInputs(m_inputs);
+        Logger.processInputs("Feeder", m_inputs);
+    }
+    public void setFeederPercentage (double percent) {
+      
+        m_io.setFeederPercentage(percent);
 
     }
     
-    public void setFeederVelocity () {
+    public void setFeederVelocity (double velocity) {
 
+        m_io.setFeederVelocity (velocity);
     }
 
-    public void getFeederVelocity () {
-    
+    public double getFeederVelocity () {
+        
+        return this.m_inputs.feederVelocity;
     }
 }
