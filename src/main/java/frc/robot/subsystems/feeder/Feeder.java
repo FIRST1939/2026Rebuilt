40b52a62813a82ab5314170a50a30a829880b6a9
Maaz-Hawa <156148130+Maaz-Hawa@users.noreply.github.com>
2026-02-07 15:18:21 -0600

    feeder sysid routine

--- src/main/java/frc/robot/subsystems/feeder/Feeder.java
+++ src/main/java/frc/robot/subsystems/feeder/Feeder.java
@@ -1,21 +1,48 @@
 package frc.robot.subsystems.feeder;
 
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 public class Feeder extends SubsystemBase {
     
     private final FeederIO m_io;
     private final FeederIOInputsAutoLogged m_inputs = new FeederIOInputsAutoLogged();
+    private final SysIdRoutine sysIdRoutine;
 
     public Feeder (FeederIO io) {
         
         m_io = io;
+
+        this.sysIdRoutine = new SysIdRoutine(
+            new SysIdRoutine.Config(
+                Volts.per(Units.Second).of(FeederConstants.kSysIdRampUpTime), 
+                Volts.of(FeederConstants.kSysIdVoltageIncrement), 
+                Seconds.of(FeederConstants.kSysIdDuration)),
+
+            new SysIdRoutine.Mechanism(
+                voltage -> io.setFeederVoltage(voltage.magnitude()), 
+                log -> {
+                    log
+                        .motor("intakeRoller")
+                        .voltage(Volts.of(this.m_inputs.feederVoltage))
+                        .angularPosition(Rotations.of(this.m_inputs.feederPosition))
+                        .angularVelocity(RotationsPerSecond.of(this.m_inputs.feederVelocity));
+                },
+                this, 
+                "Intake")
+        );
     }
 
     @Override
-
     public void periodic () {
 
         m_io.updateInputs(m_inputs);
@@ -36,4 +63,25 @@ public class Feeder extends SubsystemBase {
         
         return this.m_inputs.feederVelocity;
     }
+
+    public void setFeederVoltage(double magnitude) {
+        this.m_io.setFeederVoltage(magnitude);
+    }
+
+    public Command sysIdQuasistaticForward() {
+        return sysIdRoutine.quasistatic(SysIdRoutine.Direction.kForward);
+    }
+
+    public Command sysIdQuasistaticReverse() {
+        return sysIdRoutine.quasistatic(SysIdRoutine.Direction.kReverse);
+    }
+
+    public Command sysIdDynamicForward() {
+        return sysIdRoutine.dynamic(SysIdRoutine.Direction.kForward);
+    }
+
+    public Command sysIdDynamicReverse() {
+        return sysIdRoutine.dynamic(SysIdRoutine.Direction.kReverse);
+    }
+
 }
