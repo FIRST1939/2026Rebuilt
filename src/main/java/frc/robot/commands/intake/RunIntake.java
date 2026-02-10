108d1ce195c9ecdb0cee095d240c208038b10e62
Maaz-Hawa <156148130+Maaz-Hawa@users.noreply.github.com>
2026-02-10 00:13:19 -0600

    example command

---
+++ src/main/java/frc/robot/commands/intake/RunIntake.java
@@ -0,0 +1,26 @@
+package frc.robot.commands.intake;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intake.Intake;
+
+public class RunIntake extends Command {
+    
+    private final Intake intake;
+
+    public RunIntake (Intake intake) {
+
+        this.intake = intake;
+        this.addRequirements(this.intake);
+    }
+
+    @Override
+    public void initialize () {
+
+        this.intake.setPivotVoltage(5.0);
+    }
+
+    @Override
+    public void end (boolean interrupted) {
+
+        this.intake.setRollerVoltage(0.0);
+    }
+}
