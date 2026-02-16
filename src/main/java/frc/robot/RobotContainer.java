02666e0916ae7ce7eb6e6b7331d14501bb22b322
StarbuckBarista <66986004+StarbuckBarista@users.noreply.github.com>
2026-02-16 12:43:08 -0600

    SysId Routine Triggers

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,14 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+
 import edu.wpi.first.wpilibj.simulation.BatterySim;
 import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
 
 import frc.robot.subsystems.intake.*;
@@ -27,6 +30,16 @@ public class RobotContainer {
     private final Feeder m_feeder;
     private final Shooter m_shooter;
 
+    private enum OpModes {
+        MATCH,
+        PERCENT,
+        INTAKE_CHARACTERIZATION,
+        SPINDEXER_CHARACTERIZATION,
+        FEEDER_CHARACTERIZATION,
+        SHOOTER_CHARACTERIZATION
+    }
+
+    private final LoggedDashboardChooser<OpModes> m_opModeSelector = new LoggedDashboardChooser<>("Op Mode Selector");
     private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
     public RobotContainer(boolean isReal) {
@@ -45,24 +58,62 @@ public class RobotContainer {
             m_shooter = new Shooter(new ShooterIOHardware());
         }
         
+        m_opModeSelector.addDefaultOption("Match", OpModes.MATCH);
+        m_opModeSelector.addOption("Percent", OpModes.PERCENT);
+        m_opModeSelector.addOption("Intake Characterization", OpModes.INTAKE_CHARACTERIZATION);
+        m_opModeSelector.addOption("Spindexer Characterization", OpModes.SPINDEXER_CHARACTERIZATION);
+        m_opModeSelector.addOption("Feeder Characterization", OpModes.FEEDER_CHARACTERIZATION);
+        m_opModeSelector.addOption("Shooter Characterization", OpModes.SHOOTER_CHARACTERIZATION);
+
         configureBindings();
     }
 
     private void configureBindings() {
 
-        m_driverController.a().whileTrue(new RunSpindexer(m_spindexer, -1.0));
-        m_driverController.x().whileTrue(new RunFeeder(m_feeder, 1.0));
-        m_driverController.y().whileTrue(new RunShooter(m_shooter, 0.6));
+        Trigger percentMode = new Trigger(() -> m_opModeSelector.get() == OpModes.PERCENT);
+        Trigger intakeCharacterizationMode = new Trigger(() -> m_opModeSelector.get() == OpModes.INTAKE_CHARACTERIZATION);
+        Trigger spindexerCharacterizationMode = new Trigger(() -> m_opModeSelector.get() == OpModes.SPINDEXER_CHARACTERIZATION);
+        Trigger feederCharacterizationMode = new Trigger(() -> m_opModeSelector.get() == OpModes.FEEDER_CHARACTERIZATION);
+        Trigger shooterCharacterizationMode = new Trigger(() -> m_opModeSelector.get() == OpModes.SHOOTER_CHARACTERIZATION);
 
-        /*
-        m_driverController.y().whileTrue(m_spindexer.SpindexerSysIdDynamicForward());
-        m_driverController.b().whileTrue(m_spindexer.SpindexerSysIdDynamicReverse());
+        percentMode.and(m_driverController.a()).whileTrue(new RunSpindexer(m_spindexer, -1.0));
+        percentMode.and(m_driverController.x()).whileTrue(new RunFeeder(m_feeder, 1.0));
+        percentMode.and(m_driverController.y()).whileTrue(new RunShooter(m_shooter, 0.6));
 
-        m_driverController.leftBumper().whileTrue(m_intake.RollerSysIdQuasistaticForward());
-        m_driverController.rightBumper().whileTrue(m_intake.RollerSysIdQuasistaticReverse());
-        m_driverController.leftTrigger().whileTrue(m_intake.RollerSysIdDynamicForward());
-        m_driverController.rightTrigger().whileTrue(m_intake.RollerSysIdDynamicReverse());
-        */
+        intakeCharacterizationMode.and(m_driverController.leftBumper()).whileTrue(m_intake.rollerSysIdQuasistaticForward());
+        intakeCharacterizationMode.and(m_driverController.rightBumper()).whileTrue(m_intake.rollerSysIdQuasistaticReverse());
+        intakeCharacterizationMode.and(m_driverController.leftTrigger()).whileTrue(m_intake.rollerSysIdDynamicForward());
+        intakeCharacterizationMode.and(m_driverController.rightTrigger()).whileTrue(m_intake.rollerSysIdDynamicReverse());
+
+        intakeCharacterizationMode.and(m_driverController.povUp()).whileTrue(m_intake.leftPivotSysIdQuasistaticForward());
+        intakeCharacterizationMode.and(m_driverController.povRight()).whileTrue(m_intake.leftPivotSysIdQuasistaticReverse());
+        intakeCharacterizationMode.and(m_driverController.povDown()).whileTrue(m_intake.leftPivotSysIdDynamicForward());
+        intakeCharacterizationMode.and(m_driverController.povLeft()).whileTrue(m_intake.leftPivotSysIdDynamicReverse());
+
+        intakeCharacterizationMode.and(m_driverController.y()).whileTrue(m_intake.rightPivotSysIdQuasistaticForward());
+        intakeCharacterizationMode.and(m_driverController.b()).whileTrue(m_intake.rightPivotSysIdQuasistaticReverse());
+        intakeCharacterizationMode.and(m_driverController.a()).whileTrue(m_intake.rightPivotSysIdDynamicForward());
+        intakeCharacterizationMode.and(m_driverController.x()).whileTrue(m_intake.rightPivotSysIdDynamicReverse());
+
+        spindexerCharacterizationMode.and(m_driverController.leftBumper()).whileTrue(m_spindexer.sysIdQuasistaticForward());
+        spindexerCharacterizationMode.and(m_driverController.rightBumper()).whileTrue(m_spindexer.sysIdQuasistaticReverse());
+        spindexerCharacterizationMode.and(m_driverController.leftTrigger()).whileTrue(m_spindexer.sysIdDynamicForward());
+        spindexerCharacterizationMode.and(m_driverController.rightTrigger()).whileTrue(m_spindexer.sysIdDynamicReverse());
+
+        feederCharacterizationMode.and(m_driverController.leftBumper()).whileTrue(m_feeder.sysIdQuasistaticForward());
+        feederCharacterizationMode.and(m_driverController.leftBumper()).whileTrue(m_feeder.sysIdQuasistaticReverse());
+        feederCharacterizationMode.and(m_driverController.leftBumper()).whileTrue(m_feeder.sysIdDynamicForward());
+        feederCharacterizationMode.and(m_driverController.leftBumper()).whileTrue(m_feeder.sysIdDynamicReverse());
+
+        shooterCharacterizationMode.and(m_driverController.leftBumper()).whileTrue(m_shooter.flywheelSysIdQuasistaticForward());
+        shooterCharacterizationMode.and(m_driverController.leftBumper()).whileTrue(m_shooter.flywheelSysIdQuasistaticReverse());
+        shooterCharacterizationMode.and(m_driverController.leftBumper()).whileTrue(m_shooter.flywheelSysIdDynamicForward());
+        shooterCharacterizationMode.and(m_driverController.leftBumper()).whileTrue(m_shooter.flywheelSysIdDynamicReverse());
+
+        shooterCharacterizationMode.and(m_driverController.y()).whileTrue(m_shooter.hoodSysIdQuasistaticForward());
+        shooterCharacterizationMode.and(m_driverController.b()).whileTrue(m_shooter.hoodSysIdQuasistaticReverse());
+        shooterCharacterizationMode.and(m_driverController.a()).whileTrue(m_shooter.hoodSysIdDynamicForward());
+        shooterCharacterizationMode.and(m_driverController.x()).whileTrue(m_shooter.hoodSysIdDynamicReverse());
     }
 
     public Command getAutonomousCommand() {
